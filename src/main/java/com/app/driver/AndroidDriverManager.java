38263a9cbd5de4fdabdc06cfba836a666a1639c8
DESKTOP-H1UH67O\Dinesh <dineshkumar.subramanian@viacom18.com>
2023-05-13 00:31:59 +0530

    Added few folders and maven dependency

---
+++ src/main/java/com/app/driver/AndroidDriverManager.java
@@ -0,0 +1,12 @@
+package com.app.driver;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+
+/**
+ * This class is responsible for managing Android drivers,
+ * which are used to automate mobile application testing on Android devices.
+ */
+public class AndroidDriverManager implements IDriver {
+
+}
---
+++ src/main/java/com/app/driver/IDriver.kt
@@ -0,0 +1,5 @@
+package com.app.driver
+
+interface IDriver {
+
+}
